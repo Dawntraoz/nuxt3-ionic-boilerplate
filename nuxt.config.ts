fb3e098a0edc3a8f1ce2008a1dcaa97bb3f5b735
Dawntraoz <albasilventefu@gmail.com>
2022-07-07 16:45:52 +0200

    feat: add basic styles and font, add icon and favicon, plus basic meta tags.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,23 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  app: {
+     head: {
+      titleTemplate: '%s | Nuxtion',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'A boilerplate for creating new Nuxt 3 Ionic applications with some basic functionalities.',
+        },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' }],
+    },
+  },
+  css: [
+    '@/assets/css/main.css',
+  ]
 })
